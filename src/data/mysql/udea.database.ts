ca08b8f29b249dd716174f8d7e1f86e81bd13da6
Raul zamora <raulzamoraperez440@gmail.com>
2025-02-25 14:45:21 -0600

    dotenv

--- src/data/mysql/udea.database.ts
+++ src/data/mysql/udea.database.ts
@@ -1,15 +1,17 @@
 import { DataSource } from "typeorm";
 import { User } from "./models/User";
+import dotenv from 'dotenv'
 
+dotenv.config({path:'.env'})
 export const UdeaDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "12345",
-    database: "UDEA_DATABASE",
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT!,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
     entities: [
         User
     ],
-    synchronize: true,
+    // synchronize: true,
   });
